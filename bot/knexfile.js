6057637855949c99b2237e11cdc81a797c3419f8
Thijs Molendijk <info@thijsmolendijk.nl>
2020-09-25 22:39:01 +0200

    Skeleton of discord bot, written in TypeScript

---
+++ bot/knexfile.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+
+const db = {
+    // MikroORM
+    type: "postgresql",
+    dbName: "impostor",
+    clientUrl: process.env.DATABASE_URL,
+
+    // Knex
+    client: "postgres",
+    connection: process.env.DATABASE_URL
+};
+
+module.exports = {
+    development: db,
+    production: db
+};
